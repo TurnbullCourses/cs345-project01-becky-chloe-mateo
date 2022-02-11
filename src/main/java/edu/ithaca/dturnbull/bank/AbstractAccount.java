b4ae7410c53aa376dadcc4418f51f4c143886c68
mateoh6 <89436949+mateoh6@users.noreply.github.com>
2022-02-11 11:40:52 -0500

    Abstract account methods

--- src/main/java/edu/ithaca/dturnbull/bank/AbstractAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/AbstractAccount.java
@@ -2,32 +2,84 @@ package edu.ithaca.dturnbull.bank;
 
     abstract class Account {
         String password;
-        int balance;
+        double balance;
         
-        public Account(String password, int balance){
+        public Account(String password, double balance){
             this.password=password;
             this.balance=balance;
         }
+
+        public static boolean isAmountValid(double amount){
+            //negative amounts
+            if(amount < 0){
+                return false;
+            }
+            //more than two decimals
+            int digits;
+            double testAmount = amount;
     
-        public void withdraw (double amount){
-            balance-=amount;
+            //move left 2 decimals
+            testAmount *= 100;
+    
+            //cut off the rest of the digits
+            digits = (int) testAmount;
+            testAmount = digits;
+            testAmount /= 100;
+    
+            if(testAmount != amount){
+                return false;
+            }
+
+            return true;
+        }
+    
+        public void withdraw (double amount)throws InsufficientFundsException{
+            if(isAmountValid(amount)){
+                if (amount <= balance){
+                    balance -= amount;
+                }
+                else {
+                    throw new InsufficientFundsException("Not enough money");
+                }
+            }
+            else{
+                throw new IllegalArgumentException("Invalid Withdraw Amount");
+            }        
             
         }
-        public float checkBalance(){
+        public double checkBalance(){
             return balance;
         }
-        public void deposit(double amount){
-            balance+=amount;
+
+        public void deposit(double amount)throws InsufficientFundsException{
+            if(isAmountValid(amount)){
+                balance += amount;
+            }
+            else{
+                throw new IllegalArgumentException("Invalid Deposit Amount");
+            }
         }
-        public void transfer(Account sender, Account receiver){
-    
+
+        public void transfer(Account sender, Account receiver, double amount)throws InsufficientFundsException{
+            //check if accounts are the same
+        if(sender == receiver){
+            throw new IllegalArgumentException("Account cannot transfer to itself!");
         }
+
+        if(isAmountValid(amount)){
+            sender.withdraw(amount);
+            receiver.deposit(amount);
+        }
+        else{
+            throw new IllegalArgumentException("Invalid Transfer Amount");
+        }
+    }
+
         public String getHistory(){
             return getHistory();
         }
-        public Boolean checkPassword(){
+        public Boolean checkPassword(String password){
             return null;
-            
-        }
+         }
     
 }
