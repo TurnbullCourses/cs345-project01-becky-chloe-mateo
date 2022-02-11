4cb866a3bb93aea84d9c3e9bf879d9ed3039266d
beckylindquist <beckylindquist2018@gmail.com>
2022-02-11 11:43:51 -0500

    Added withdraw test and impl

--- src/main/java/edu/ithaca/dturnbull/bank/AbstractAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/AbstractAccount.java
@@ -1,5 +1,14 @@
 package edu.ithaca.dturnbull.bank;
 
 public class AbstractAccount {
+
+    private String email;
+    private double balance;
     
+    public void withdraw(String email, double amount) throws InsufficientFundsException{
+        if(amount < balance){
+            throw new InsufficientFundsException("Not enough money");
+            }
+        balance -= amount;
+    }
 }
