347d85142dfe7d53b26d86b335adcd79ae5fa320
mateoh6 <89436949+mateoh6@users.noreply.github.com>
2022-02-11 11:34:02 -0500

    abstract account

--- src/main/java/edu/ithaca/dturnbull/bank/AbstractAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/AbstractAccount.java
@@ -1,5 +1,33 @@
 package edu.ithaca.dturnbull.bank;
 
-public class AbstractAccount {
+    abstract class Account {
+        String password;
+        int balance;
+        
+        public Account(String password, int balance){
+            this.password=password;
+            this.balance=balance;
+        }
+    
+        public void withdraw (double amount){
+            balance-=amount;
+            
+        }
+        public float checkBalance(){
+            return balance;
+        }
+        public void deposit(double amount){
+            balance+=amount;
+        }
+        public void transfer(Account sender, Account receiver){
+    
+        }
+        public String getHistory(){
+            return getHistory();
+        }
+        public Boolean checkPassword(){
+            return null;
+            
+        }
     
 }
