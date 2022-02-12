13a033115b4aca5c52db0d8828d42ef320c01755
beckylindquist <beckylindquist2018@gmail.com>
2022-02-12 12:50:57 -0500

    added withdraw tests

--- src/main/java/edu/ithaca/dturnbull/bank/SavingsAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/SavingsAccount.java
@@ -1,5 +1,12 @@
 package edu.ithaca.dturnbull.bank;
 
 public class SavingsAccount extends AbstractAccount {
+
+    double maxWithdraw = 1000;
     
+    void withdraw(String email, double amount) throws IllegalArgumentException{
+        if(maxWithdraw >amount){
+            throw new IllegalArgumentException("exceeds max withdraw");
+        }
+    }
 }
