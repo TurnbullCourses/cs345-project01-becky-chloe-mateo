34abe19be0a5863f02790a572130bcb95c1c9d41
mateoh6 <89436949+mateoh6@users.noreply.github.com>
2022-02-13 22:21:34 -0500

    CreateAccount

--- src/main/java/edu/ithaca/dturnbull/bank/BankTeller.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankTeller.java
@@ -1,5 +1,13 @@
 package edu.ithaca.dturnbull.bank;
 
-public class BankTeller extends AbstractBank{
-    
+public class BankTeller extends AbstractBank {
+    public AbstractAccount createAccount(String typeOfAccount, String password, double balance, double dailyMax) {
+        if (typeOfAccount == "Savings") {
+            SavingsAccount savingsAccount = new SavingsAccount(password, balance, dailyMax);
+            return savingsAccount;
+        } else {
+            CheckingAccount checkingAccount = new CheckingAccount(password, balance);
+            return checkingAccount;
+        }
+    }
 }
