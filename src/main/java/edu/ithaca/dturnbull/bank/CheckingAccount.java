18b6e9b6b8394af56c9fb110f9354a350438f702
mateoh6 <89436949+mateoh6@users.noreply.github.com>
2022-02-13 22:01:37 -0500

    Checking account

--- src/main/java/edu/ithaca/dturnbull/bank/CheckingAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/CheckingAccount.java
@@ -1,4 +1,57 @@
 package edu.ithaca.dturnbull.bank;
 public class CheckingAccount extends AbstractAccount{
+
+    public CheckingAccount(String password, double balance) {
+        super(password, balance);
+        
+    }
+    public static boolean isAmountValid(double amount) {
+        // negative amounts
+        if (amount < 0) {
+            return false;
+        }
+        // more than two decimals
+        int digits;
+        double testAmount = amount;
+
+        // move left 2 decimals
+        testAmount *= 100;
+
+        // cut off the rest of the digits
+        digits = (int) testAmount;
+        testAmount = digits;
+        testAmount /= 100;
+
+        if (testAmount != amount) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void withdraw(double amount) throws InsufficientFundsException {
+
+    }
+
+    public double checkBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) throws InsufficientFundsException {
+
+    }
+
+    public void transfer(AbstractAccount sender, AbstractAccount receiver, double amount) throws InsufficientFundsException {
+
+    }
+
+    public String getHistory() {
+        return null;
+    }
+
+    public Boolean checkPassword(String accountNumber) {
+        return null;
+
+    }
     
 }
