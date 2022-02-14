8ac12e378deefa79c5597ca801906af1be251dad
mateoh6 <89436949+mateoh6@users.noreply.github.com>
2022-02-13 21:59:05 -0500

    Savings account

--- src/main/java/edu/ithaca/dturnbull/bank/SavingsAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/SavingsAccount.java
@@ -1,11 +1,25 @@
 package edu.ithaca.dturnbull.bank;
 
 public class SavingsAccount extends AbstractAccount {
+
     double dailyMax;
 
     public SavingsAccount(String password, double balance, double dailyMax) {
         super(password, balance);
-        
+        this.dailyMax=dailyMax;
+    }
+
+    public void withdraw(double amount) throws IllegalArgumentException {
+        if(amount>dailyMax){
+            throw new IllegalArgumentException("exceeds daily maximum");
+        }
+        balance -= amount;
+    }
+
+    public double compoundInterest(double interest,int numOfCompund){
+        double principal=balance;
+        double compoundedInterest=((principal)*Math.pow((1+interest),numOfCompund)-principal);
+        return compoundedInterest;
     }
 
 
