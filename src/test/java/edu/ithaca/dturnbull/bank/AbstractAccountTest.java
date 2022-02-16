f35b86522b81b0bf8406da34df15e4f4269f44c9
Chloe Choe <cchoe@ithaca.edu>
2022-02-16 11:26:23 -0500

    failing tests

--- src/test/java/edu/ithaca/dturnbull/bank/AbstractAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/AbstractAccountTest.java
@@ -11,14 +11,28 @@ class AbstractAccountTest {
         CheckingAccount checkAcc = new CheckingAccount("a@b.com", 200); //Checking
         checkAcc.withdraw(100); //Normal Withdraw
 
-        assertEquals(100, checkAcc.getBalance(), 0.001);
+        assertEquals(100, checkAcc.checkBalance(), 0.001);
         assertThrows(InsufficientFundsException.class, () -> checkAcc.withdraw(300)); //Insufficient Funds
 
         SavingsAccount savingAcc = new SavingsAccount("a@b.com", 1500); //Savings
         savingAcc.withdraw(100);
 
-        assertEquals(1400, checkAcc.getBalance(), 0.001);
+        assertEquals(1400, checkAcc.checkBalance(), 0.001);
         assertThrows(IllegalArgumentException.class, () -> savingAcc.withdraw(1005)); //Over Max Withdrawal
+    }
+
+    @Test
+    void depositTest() throws IllegalArgumentException{
+        CheckingAccount checkAcc = new CheckingAccount("a@b.com", 200); //Checking
+        checkAcc.deposit(100); //Normal deposit
+
+        assertEquals(300, checkAcc.checkBalance(), 0.001);
+        assertThrows(IllegalArgumentException.class, () -> checkAcc.deposit(-1)); //Insufficient Funds
+
+        SavingsAccount savingAcc = new SavingsAccount("a@b.com", 100); //Savings
+        savingAcc.deposit(100);
+
+        assertEquals(200, checkAcc.checkBalance(), 0.001);
     }
 }
 
