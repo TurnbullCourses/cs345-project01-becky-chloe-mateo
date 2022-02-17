abe8b5e117fe5a21266d68d9ceacad72739e8ffc
beckylindquist <beckylindquist2018@gmail.com>
2022-02-17 15:30:55 -0500

    Added check password impl to customer

--- src/main/java/edu/ithaca/dturnbull/Customer.java
+++ src/main/java/edu/ithaca/dturnbull/Customer.java
@@ -1,5 +1,17 @@
 package edu.ithaca.dturnbull;
 
 public class Customer {
+    String password;
+    double balance;
+    public Customer(String password, double balance) {
+        this.password = password;
+        this.balance = balance;
+    }
+    public boolean checkPassword(String passInput){
+        if(password == passInput){
+            return true;
+        }
+        return false;
+    }
     
 }
