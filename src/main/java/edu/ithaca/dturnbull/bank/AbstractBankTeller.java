734ecf11f0c3431d48975dbbe67a941215642fa3
Chloe Choe <cchoe@ithaca.edu>
2022-02-25 11:31:07 -0500

    abstract bank teller implementations

--- src/main/java/edu/ithaca/dturnbull/bank/AbstractBankTeller.java
+++ src/main/java/edu/ithaca/dturnbull/bank/AbstractBankTeller.java
@@ -1,40 +1,81 @@
 package edu.ithaca.dturnbull.bank;
 
-abstract class AbstractBankTeller {
+abstract class AbstractBankTeller  {
          
-    public static AbstractAccount createAccount(String typeOfAccount, String password, double balance, double dailyMax) {
-        if (typeOfAccount == "Savings") {
-            AbstractAccount savingsAccount = new SavingsAccount(password, balance, dailyMax);
-            return savingsAccount;
+    double balance;
+    static String transactionHistory;
+    String password;
+    
+    public static boolean isAmountValid(double amount) {
+        // negative amounts
+        if (amount < 0) {
+            return false;
         }
-       else {
-                AbstractAccount checkingAccount = new CheckingAccount(password, balance);
-                return checkingAccount;
-            }
+        // more than two decimals
+        int digits;
+        double testAmount = amount;
+    
+        // move left 2 decimals
+        testAmount *= 100;
+    
+        // cut off the rest of the digits
+        digits = (int) testAmount;
+        testAmount = digits;
+        testAmount /= 100;
+    
+        if (testAmount != amount) {
+            return false;
         }
     
-        public void closeAccount(){
-
-        }
-        public void getBalance(){
-
-        }
-        public void deposit(){
-
-        }
-        public void withdraw(){
-
-        }
-        public void transfer(){
-
+        return true;
         }
 
-        public void checkHistory(){
-
-        }
-
-        public void checkPassword(){
-            
-        }
-
+    public double getBalance(){
+        return balance; 
     }
+
+    public void deposit(double amount) throws InsufficientFundsException {
+        if(isAmountValid(amount)){
+            balance += amount;
+            transactionHistory+="Deposit: " + amount + ","; //Add to transaction history
+        }
+        else{
+            throw new IllegalArgumentException("Invalid Deposit Amount");
+        }
+    }
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if(amount>balance){
+            throw new InsufficientFundsException("not enough funds");
+        }
+        balance -= amount;
+        transactionHistory+="Withdraw: " + amount + ","; //Add to transaction history
+    }
+    public static void transfer(AbstractAccount sender, AbstractAccount receiver, double amount) throws InsufficientFundsException {
+        //check if accounts are the same
+        if(sender == receiver){
+            throw new IllegalArgumentException("Account cannot transfer to itself!");
+        }
+
+        if(isAmountValid(amount)){
+            sender.withdraw(amount);
+            receiver.deposit(amount);
+            transactionHistory+="Transfer: " + amount + "from" + sender + "to" + receiver + ","; //Add to transaction history
+        }
+        else{
+            throw new IllegalArgumentException("Invalid Transfer Amount");
+        }
+    }
+
+    public String checkHistory(){
+        return transactionHistory;
+    }
+
+    public boolean checkPassword(String passInput){
+        if(password == passInput){
+            return true;
+        }
+        return false;
+    }
+    
+
+}
