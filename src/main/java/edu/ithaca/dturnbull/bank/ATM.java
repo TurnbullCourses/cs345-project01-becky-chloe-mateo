c1d58dfa29d1d8c73ea0b22fc7e3d1a6703d543f
mateoh6 <89436949+mateoh6@users.noreply.github.com>
2022-02-25 11:33:40 -0500

    ATM methods

--- src/main/java/edu/ithaca/dturnbull/bank/ATM.java
+++ src/main/java/edu/ithaca/dturnbull/bank/ATM.java
@@ -1,5 +1,25 @@
 package edu.ithaca.dturnbull.bank;
 
-public class ATM {
+public class ATM extends AbstractBankTeller {
     
+    public void getBalance(){
+
+    }
+    public void deposit(){
+
+    }
+    public void withdraw(){
+
+    }
+    public void transfer(){
+
+    }
+
+    public void checkHistory(){
+
+    }
+
+    public void checkPassword(){
+        
+    }
 }
