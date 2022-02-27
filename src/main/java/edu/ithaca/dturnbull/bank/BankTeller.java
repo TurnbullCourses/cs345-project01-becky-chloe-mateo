a40ac39dc3444f248a5765e3d522da598bc2c83a
mateoh6 <89436949+mateoh6@users.noreply.github.com>
2022-02-27 17:14:21 -0500

    Added customer parameter to create account

--- src/main/java/edu/ithaca/dturnbull/bank/BankTeller.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankTeller.java
@@ -2,13 +2,13 @@ package edu.ithaca.dturnbull.bank;
 
 public class BankTeller extends AbstractBankTeller {
 
-    public static AbstractAccount createAccount(String typeOfAccount, String password, double balance, double dailyMax) {
+    public static AbstractAccount createAccount(Customer customer,String typeOfAccount, String password, double balance, double dailyMax) {
         if (typeOfAccount == "Savings") {
-            AbstractAccount savingsAccount = new SavingsAccount(null, balance, dailyMax);
+            AbstractAccount savingsAccount = new SavingsAccount(customer, balance, dailyMax);
             return savingsAccount;
         } 
         else {
-            AbstractAccount checkingAccount = new CheckingAccount(balance);
+            AbstractAccount checkingAccount = new CheckingAccount(customer, balance);
             return checkingAccount;
         }
     }
