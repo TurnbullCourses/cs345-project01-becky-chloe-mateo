c8c120c2ce92cddfb1fcfa81a416afa0dc2282f8
mateoh6 <89436949+mateoh6@users.noreply.github.com>
2022-02-27 16:09:27 -0500

    Request Report

--- src/main/java/edu/ithaca/dturnbull/bank/Admin.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Admin.java
@@ -1,4 +1,20 @@
 package edu.ithaca.dturnbull.bank;
 public class Admin extends AbstractBank {
-    
+    private String accountName;
+    private String password;
+
+    public Admin(String accountName, String password){
+        this.accountName=accountName;
+        this.password=password;
+    }
+
+    public String requestReport(AbstractAccount account){
+        if(account.getSuspiciousActivity()==true){
+            System.out.print("There has been suspicious activity on this account");
+            return AbstractAccount.transactionHistory;
+        }
+        
+        return null;
+        
+    }
 }
